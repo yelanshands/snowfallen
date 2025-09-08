056409e9b465eddc36dc2688d8e6873227cf5ce7
yindy cam <cindyyam8@gmail.com>
2025-09-07 22:13:17 -0700

    more work on gameplay
    smoother movement and camera
    added crosshair

--- frappecchino/home.gd
+++ frappecchino/home.gd
@@ -10,12 +10,15 @@ extends CanvasLayer
 
 var rot_x = 0
 var rot_y = 0
-var LOOKAROUND_SPEED = 0.005
+var cam_sens = 0.005
 var logged_x = 0.0
 var logged_y = 0.0
 var target_rot_x = 0.0
 var target_rot_y = 0.0
 	
+func _init():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _process(_delta):
 	buttonHovered(play)
 	buttonHovered(settings)
@@ -25,22 +28,16 @@ func _process(_delta):
 	var current_y = get_viewport().get_mouse_position()[1]
 	
 	# Compute the difference between the target mouse position and the current rotation
-	target_rot_x += (current_x - logged_x) * LOOKAROUND_SPEED
-	target_rot_y += (current_y - logged_y) * LOOKAROUND_SPEED
+	target_rot_x += (current_x - logged_x) * cam_sens
+	target_rot_x = clamp(target_rot_x, 0.6, 1.2)
 	
-	if target_rot_x < 0.6:
-		target_rot_x = 0.6
-	elif target_rot_x > 1.2: 
-		target_rot_x = 1.2
-	if target_rot_y < -0.4:
-		target_rot_y = -0.4
-	elif target_rot_y > 0.4:
-		target_rot_y = 0.4
+	target_rot_y += (current_y - logged_y) * cam_sens
+	target_rot_y = clamp(target_rot_y, -0.4, 0.4)
 
 	# Interpolate the current rotation towards the target rotation smoothly
 	rot_x = lerp_angle(rot_x, target_rot_x, 0.005)
 	rot_y = lerp_angle(rot_y, target_rot_y, 0.005)
-	print("x:" + str(rot_x) + "    y: " + str(rot_y))
+	#print("x:" + str(rot_x) + "    y: " + str(rot_y))
 	
 	logged_x = current_x
 	logged_y = current_y
@@ -48,25 +45,6 @@ func _process(_delta):
 	camera.transform.basis = Basis() # reset rotation
 	camera.rotate_object_local(Vector3(0, -1, 0), rot_x) # first rotate in Y
 	camera.rotate_object_local(Vector3(-1, 0, 0), rot_y) # then rotate in X
-		
-#
-#
-#func _input(event):
-	#if event is InputEventMouseMotion:
-		#
-		# modify accumulated mouse rotation
-		#rot_x += event.screen_relative.x * LOOKAROUND_SPEED
-		#rot_y += event.screen_relative.y * LOOKAROUND_SPEED
-		#print("x:" + str(rot_x) + "    y: " + str(rot_y))
-		#camera.transform.basis = Basis() # reset rotation
-		#camera.rotate_object_local(Vector3(0, -1, 0), rot_x) # first rotate in Y
-		#camera.rotate_object_local(Vector3(-1, 0, 0), rot_y) # then rotate in X
-#func _input(event):
-	#if event is InputEventMouseMotion:
-		## 'event.relative' provides the change in mouse position since the last frame.
-		#print("Mouse moved by: ", event.relative) 
-		## 'event.position' provides the current absolute mouse position on the screen.
-		#print("Mouse position: ", event.position)
 
 func do_tween(object: Object, property: String, value: Variant, duration: float):
 	var tween = create_tween()
@@ -77,6 +55,9 @@ func buttonHovered(button: TextureButton):
 		do_tween(button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 	else:
 		do_tween(button, "scale", Vector2.ONE, tween_duration)
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
---
+++ frappecchino/player.gd
@@ -0,0 +1,78 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = $SpringArmPivot/PlayerCamera
+@onready var spring_arm: SpringArm3D = $SpringArmPivot/SpringArm3D
+@onready var spring_pos: Node3D = $SpringArmPivot/SpringArm3D/SpringPosition
+@onready var crosshair: TextureRect = $/root/Game/Control/Crosshair
+
+@export var fov: float = 75.0
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")*9.8
+var default_speed = 40.0  
+var speed = default_speed
+var jump_speed = 40.0
+var cam_sens = 0.0025
+var sprint_mult = 1.25
+var crouch_mult = 0.75
+var lerp_value = 8.0
+var crosshair_size = 60.0
+var crosshair_sprint = 40.0
+var crosshair_crouch = 80.0
+
+func _init():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event.is_action_pressed("left_click"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			get_viewport().set_input_as_handled()
+
+func _process(delta):
+	camera.position = lerp(camera.position, spring_pos.position, delta*lerp_value)
+
+func get_input():
+	var input = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	
+	if Input.is_action_pressed("sprint"):
+		speed = default_speed * sprint_mult
+		camera.fov = lerp(camera.fov, fov * sprint_mult, 0.2)
+		crosshair.set_size(Vector2(lerp(crosshair.size.x, crosshair_sprint, 0.3), lerp(crosshair.size.y, crosshair_sprint, 0.3)))
+	elif Input.is_action_pressed("crouch"):
+		speed = default_speed * crouch_mult
+		camera.fov = lerp(camera.fov, fov * crouch_mult, 0.2)
+		crosshair.set_size(Vector2(lerp(crosshair.size.x, crosshair_crouch, 0.3), lerp(crosshair.size.y, crosshair_crouch, 0.3)))
+	else:
+		speed = default_speed
+		camera.fov = lerp(camera.fov, fov, 0.2)
+		crosshair.set_size(Vector2(lerp(crosshair.size.x, crosshair_size, 0.3), lerp(crosshair.size.y, crosshair_size, 0.3)))
+		
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		
+	crosshair.position.x = DisplayServer.window_get_size()[0]/2-(crosshair.size.x/2)
+	crosshair.position.y = DisplayServer.window_get_size()[1]/2-(crosshair.size.y/2)
+	
+	velocity.x = -movement_dir.x * speed
+	velocity.z = -movement_dir.z * speed
+	
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	get_input()
+	move_and_slide()
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * cam_sens)
+
+		camera.rotation.x -= event.relative.y * cam_sens
+		camera.rotation.x = clamp(camera.rotation.x, -PI/4, PI/2) 
+		
+	if event.is_action_pressed("wheel_up"):
+		spring_arm.spring_length -= 1
+	if event.is_action_pressed("wheel_down"):
+		spring_arm.spring_length += 1
+	spring_arm.spring_length = clamp(spring_arm.spring_length, 10, 45)
